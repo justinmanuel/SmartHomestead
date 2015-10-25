0ec617fd08cc3c0c229987bbd7c888f5e6c3dac1
Justin Manuel <justin.manuel@incenergy.com>
2015-10-24 19:16:24 -0500

    Start saving changes to the database

--- Arduino/GardenMonitor/GardenMonitor.ino
+++ Arduino/GardenMonitor/GardenMonitor.ino
@@ -1,10 +1,31 @@
+int index = 0;
+char frame[20];
+char prevByte;
+
+void sendMoisture();
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial1.begin(9600);
     
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial1.available() > 0)
+  {
+    byte inByte = Serial1.read();
+    if ( inByte == 1)
+    {
+      int moisture = analogRead(1);
+      Serial1.print(moisture, DEC);
+    }
+    else if( inByte == 2 )
+    {
+      byte state = digitalRead(2);
+      Serial1.print(state);
+    }
+    else if ( inByte == 3) digitalWrite(2, HIGH);
+    else if ( inByte == 4) digitalWrite(2, LOW);
+  }
 }
--- Python/GardenDataServer.py
+++ Python/GardenDataServer.py
@@ -4,12 +4,17 @@ import serial
 import MySQLdb
 import time
 
-db = MySQLdb.connect("localhost", "monitor", "password", "")
+db = MySQLdb.connect("localhost", "monitor", "password", "moisture")
 curs = db.cursor()
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 while True:
+    ser.write('\x01')
+    time.sleep(2)
+    moisture = int(ser.read(ser.inWaiting()))
+    ser.write('\x02')
+    time.sleep(2)
+    state = int(ser.read(ser.inWaiting()))
     curs.execute("""INSERT INTO moisture
-                 values(CURRENT_DATE() - INTERVAL 1 DAY, NOW(), 0, 0)""")
+                 values(CURRENT_DATE() - INTERVAL 1 DAY, NOW(), state, moisture)""")
     time.sleep(60)
-    
